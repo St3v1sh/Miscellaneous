e3d29b660b2ee54784e3f7e6a717fa09770a59c4
St3v1sh <sky3947@rit.edu>
2022-04-21 22:03:34 -0400

    Fixes

--- JavaScript Playground/autoredeem.js
+++ JavaScript Playground/autoredeem.js
@@ -3,15 +3,15 @@
  * redeem is in the constant `TARGET_REDEEM`. Change `maxIterations` to the
  * desired number of redeems. Don't forget to un-comment `btnConfirm.click();`!
  * This code also assumes that the target redeem has no cooldown.
- * 
+ *
  * @author  sky3947@rit.edu
- * @since   11.2.2021
+ * @since   4.21.2022
  */
 
 const TARGET_REDEEM = 'Hydrate';
 
-const REDEEMS_CLASS_NAME = 'Layout-sc-nxg1ff-0 cmslTX';
-const CONFIRM_BUTTONS_CLASS_NAME = 'Layout-sc-nxg1ff-0 hClGgz';
+const REDEEMS_CLASS_NAME = 'Layout-sc-nxg1ff-0 eUGccq rewards-list';
+const CONFIRM_BUTTONS_CLASS_NAME = 'Layout-sc-nxg1ff-0 dTEEED';
 
 const $className = (className) => document.getElementsByClassName(className);
 const $qSelectorAll = (query) => document.querySelectorAll(query);
@@ -29,58 +29,58 @@ const States = {
   WAITING_FOR_REDEEM_BUTTON: 1,
   WAITING_FOR_CONFIRM_BUTTON: 2,
 };
-const nextState = (state) => (state+1) % Object.keys(States).length;
+const nextState = (state) => (state + 1) % Object.keys(States).length;
 
 const autoClick = (state) => {
-  if(iteration >= maxIterations) return;
-  
-  switch(state) {
+  if (iteration >= maxIterations) return;
+
+  switch (state) {
     case States.INITIAL:
       console.log('Emergency stop: use `q()` to stop\nopening rewards div');
-      if($className(REDEEMS_CLASS_NAME).length > 0) {
+      if ($className(REDEEMS_CLASS_NAME).length > 0) {
         autoClicker(state);
         return;
       }
-      
+
       btnOpenRedeems.click();
       autoClicker(nextState(state));
       return;
-      
+
     case States.WAITING_FOR_REDEEM_BUTTON:
       console.log(`waiting for redeem button "${TARGET_REDEEM}"`);
-      let redeems = Array.from($className(REDEEMS_CLASS_NAME));
-      redeems = redeems.filter(redeem => redeem.children.length > 1 && redeem.children[1].innerText === TARGET_REDEEM);
-      if(redeems.length === 0) {
+      let redeems = Array.from($className(REDEEMS_CLASS_NAME)[0].children);
+      redeems = redeems.filter(redeem => redeem.children.length > 0 && redeem.children[0].children.length > 1 && redeem.children[0].children[1].innerText === TARGET_REDEEM);
+      if (redeems.length === 0) {
         autoClicker(state);
         return;
       }
-      
-      btnRedeem = redeems[0].children[0];
+
+      btnRedeem = redeems[0].children[0].children[0];
       btnRedeem.click();
       autoClicker(nextState(state));
       return;
-      
+
     case States.WAITING_FOR_CONFIRM_BUTTON:
       console.log('waiting for confirm redeem button');
       let confirmButtonsDivs = $className(CONFIRM_BUTTONS_CLASS_NAME);
-      if(confirmButtonsDivs.length === 0) {
+      if (confirmButtonsDivs.length === 0) {
         autoClicker(state);
         return;
       }
-      
+
       let confirmButtons = Array.from(confirmButtonsDivs).filter(elm => elm.innerHTML.includes('>Redeem<'));
-      if(confirmButtons.length === 0) {
+      if (confirmButtons.length === 0) {
         autoClicker(state);
         return;
       }
-      
+
       btnConfirm = confirmButtons[0].children[0];
       // btnConfirm.click();
       btnOpenRedeems.click();
       iteration++;
       autoClicker(nextState(state));
       return;
-      
+
     default:
       console.error('Error: Invalid state');
   }
